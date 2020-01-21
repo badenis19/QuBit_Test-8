67a7510ea70ef4a2344168e544dc777bcaea7538
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-01-21 17:20:21 +0000

    first 4 passed the test

--- Answer.js
+++ Answer.js
@@ -1,3 +1,21 @@
 var $ = function (selector) {
-	
+
+    
+    // var elements = [...document.getElementsByTagName('div')];
+ 
+    // var elements = [...document.getElementsByTagName('img')];
+
+    // var elements = [document.getElementById('some_id')];
+
+    // var elements = [...document.getElementsByClassName('some_class')];
+
+    // $("input#some_id") - Should return an empty array 
+    var elements = [document.getElementById('someinput some_id')];
+
+    // $("div#some_id.some_class") - Should return 1 DIV 
+
+    // $("div.some_class#some_id") - Should return 1 DIV
+
+
+    return elements;
 }
