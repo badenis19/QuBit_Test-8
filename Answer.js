80de857331648efa25c81b6b52eb5217c4c2fd81
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-01-30 23:28:44 +0000

    trying other logic

--- Answer.js
+++ Answer.js
@@ -1,52 +1,89 @@
 var $ = function (selector) {
-  var elements = [];
+  let elements = []
 
-  if (selector){
-    
+  const selectorString = String(selector)
 
-    // cut down the selector to extract class / id and tag
-    let selector_tag = selector.match(/^(\w+)\W/)[1];
+  if (selectorString.startsWith('#')) {
+      elements = [
+          ...elements,
+          document.getElementById(
+              selectorString.substring(1)
+          )
+      ]
+  }
 
-    let res = [...document.getElementsByTagName(selector_tag)];
-    console.log(res)
-    // console.log("tags: " , selector_tag)
-    // let selector_ids = selector.match(/\#(\w+)/)[1];
-    // console.log("ids:" , selector_ids)
-    // let selector_class = selector.match(/\.(\w+)/)[1];
-    // console.log("class:" , selector_class)
+  if (selectorString.startsWith('.')) {
+      elements = [
+          ...elements,
+          ...document.getElementsByClassName(
+              selectorString.substring(1)
+          )
+      ]
+  }
 
-
-  } else {
-    console.log("enter selectors")
+  if (selectorString.includes('.')) {
+      elements = [
+          ...elements,
+          ...document.getElementsByTagName(
+              selectorString.split('.')[0]
+          )
+      ]
   }
 
 
-  
-
-
-
-
-
-// $("div") - Should return 2 DIVs
-  // elements = [...document.getElementsByTagName(selector)];
-  // console.log(elements);
-
-
-// $("img.some_class") - Should return 1 IMG 
-// $("#some_id") - Should return 1 DIV 
-// $(".some_class") - Should return 1 DIV and 1 IMG 
-// $("input#some_id") - Should return an empty array 
-// $("div#some_id.some_class") - Should return 1 DIV 
-// $("div.some_class#some_id") - Should return 1 DIV
-
-  
-
-  return elements;
+  return elements.filter(element => !!element)
 }
 
-$("div#some_id.some_class");
+// var $ = function (selector) {
+//   var elements = [];
 
-// pseudo code
+//   if (selector){
+    
 
-// pull all nodes in the document 
-// recursively go throu all the nodes in a loop lloking for selector 
+//     // cut down the selector to extract class / id and tag
+//     let selector_tag = selector.match(/^(\w+)\W/)[1];
+//     let selector_tag = selector.match(/^(\w+)\W/)[1];
+//     console.log("res:::",  )
+//     let res = [...document.getElementsByTagName(selector_tag)];
+//     console.log(res)
+//     // console.log("tags: " , selector_tag)
+//     // let selector_ids = selector.match(/\#(\w+)/)[1];
+//     // console.log("ids:" , selector_ids)
+//     // let selector_class = selector.match(/\.(\w+)/)[1];
+//     // console.log("class:" , selector_class)
+
+
+//   } else {
+//     console.log("enter selectors")
+//   }
+
+
+  
+
+
+
+
+
+// // $("div") - Should return 2 DIVs
+//   // elements = [...document.getElementsByTagName(selector)];
+//   // console.log(elements);
+
+
+// // $("img.some_class") - Should return 1 IMG 
+// // $("#some_id") - Should return 1 DIV 
+// // $(".some_class") - Should return 1 DIV and 1 IMG 
+// // $("input#some_id") - Should return an empty array 
+// // $("div#some_id.some_class") - Should return 1 DIV 
+// // $("div.some_class#some_id") - Should return 1 DIV
+
+  
+
+//   return elements;
+// }
+
+// $("div#some_id.some_class");
+
+// // pseudo code
+
+// // pull all nodes in the document 
+// // recursively go throu all the nodes in a loop lloking for selector 
