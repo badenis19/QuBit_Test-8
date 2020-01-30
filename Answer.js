c5497f1ade25077b497f5805b9e94b58cb190bc4
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-01-30 23:25:38 +0000

    collecting different type of element from parameters

--- Answer.js
+++ Answer.js
@@ -1,21 +1,52 @@
 var $ = function (selector) {
+  var elements = [];
 
+  if (selector){
     
-    // var elements = [...document.getElementsByTagName('div')];
- 
-    // var elements = [...document.getElementsByTagName('img')];
 
-    // var elements = [document.getElementById('some_id')];
+    // cut down the selector to extract class / id and tag
+    let selector_tag = selector.match(/^(\w+)\W/)[1];
 
-    // var elements = [...document.getElementsByClassName('some_class')];
-
-    // $("input#some_id") - Should return an empty array 
-    var elements = [document.getElementById('someinput some_id')];
-
-    // $("div#some_id.some_class") - Should return 1 DIV 
-
-    // $("div.some_class#some_id") - Should return 1 DIV
+    let res = [...document.getElementsByTagName(selector_tag)];
+    console.log(res)
+    // console.log("tags: " , selector_tag)
+    // let selector_ids = selector.match(/\#(\w+)/)[1];
+    // console.log("ids:" , selector_ids)
+    // let selector_class = selector.match(/\.(\w+)/)[1];
+    // console.log("class:" , selector_class)
 
 
-    return elements;
+  } else {
+    console.log("enter selectors")
+  }
+
+
+  
+
+
+
+
+
+// $("div") - Should return 2 DIVs
+  // elements = [...document.getElementsByTagName(selector)];
+  // console.log(elements);
+
+
+// $("img.some_class") - Should return 1 IMG 
+// $("#some_id") - Should return 1 DIV 
+// $(".some_class") - Should return 1 DIV and 1 IMG 
+// $("input#some_id") - Should return an empty array 
+// $("div#some_id.some_class") - Should return 1 DIV 
+// $("div.some_class#some_id") - Should return 1 DIV
+
+  
+
+  return elements;
 }
+
+$("div#some_id.some_class");
+
+// pseudo code
+
+// pull all nodes in the document 
+// recursively go throu all the nodes in a loop lloking for selector 
