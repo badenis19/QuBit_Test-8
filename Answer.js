bc7057e78dd711f2dedde39749d7f5751782828c
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-02-03 22:37:08 +0000

    improved solution

--- Answer.js
+++ Answer.js
@@ -1,38 +1,87 @@
-var $ = function (selector) {
-  let elements = []
+function findMatches(input, regex) {
 
-  const selectorString = String(selector)
-
-  if (selectorString.startsWith('#')) {
-      elements = [
-          ...elements,
-          document.getElementById(
-              selectorString.substring(1)
-          )
-      ]
-  }
-
-  if (selectorString.startsWith('.')) {
-      elements = [
-          ...elements,
-          ...document.getElementsByClassName(
-              selectorString.substring(1)
-          )
-      ]
-  }
-
-  if (selectorString.includes('.')) {
-      elements = [
-          ...elements,
-          ...document.getElementsByTagName(
-              selectorString.split('.')[0]
-          )
-      ]
-  }
-
-
-  return elements.filter(element => !!element)
+    const items = new Set() // creating new set 
+    while (input.length > 0) { // when the argument is not empty
+        if (regex.test(input)) { // check input if the regex exists, if true 
+            const [item] = input.match(regex); // using destructuring to store the result in the variable item  // const item = input.match(regex)[0]
+            items.add(item); // add item to the set 
+            input = input.replace(item, ''); // remove item 
+            continue // break flow of the loop to next loop interation
+        }
+        input = input.substring(1); // else of the if statement - removes the first letter of input if regex doesn't exist in input
+    }
+    return [...items]
 }
+
+function intersection(sets) {
+
+    if (sets.length === 1)
+        return sets[0]
+
+    let _intersection = new Set()
+
+    for (let iSets = 0; iSets < sets.length; iSets++) {
+        for (let elem of sets[iSets]) {
+            for (let setI = 0; setI < sets.length; setI++) {
+                if (sets[iSets] === sets[setI]) {
+                    continue;
+                }
+
+                if (!sets[setI].has(elem)) {
+                    continue;
+                }
+
+                if (setI === sets.length - 1) {
+                    _intersection.add(elem)
+                }
+            }
+        }
+    }
+
+    return _intersection
+}
+
+// Regexes
+const CLASS_REGEX = /\.[a-zA-Z-_]+/
+const ID_REGEX = /\#[a-zA-Z-_]+/
+const TAG_REGEX = /div|input|img/
+
+// main entry function 
+var $ = function (selector) {
+
+    const idSet = new Set(findMatches(selector, ID_REGEX).flatMap($id))
+    const classSet = new Set(findMatches(selector, CLASS_REGEX).flatMap($class))
+    const tagSet = new Set(findMatches(selector, TAG_REGEX).flatMap($tag))
+
+    const elements = intersection(
+        [idSet, classSet, tagSet].filter(set => set.size > 0)
+    )
+
+    return [...elements]
+}
+
+const $id = (selector) => {
+    const element = document.getElementById(String(selector).substring(1))
+
+    return element ? [element] : []
+}
+
+const $class = (selector) => {
+    return [
+        ...document.getElementsByClassName(
+            String(selector).substring(1)
+        )
+    ]
+}
+
+const $tag = (selector) => {
+    return [
+        ...document.getElementsByTagName(
+            String(selector)
+        )
+    ]
+}
+
 
 // var $ = function (selector) {
 //   var elements = [];
